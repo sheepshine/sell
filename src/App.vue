bf61fd627ddf76d5671ec203200912fefdc74c34
yangchaoben <842293890@qq.com>
2017-03-02 23:07:53 +0800

    2017-03-02 23:07:52

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div class="content">
+      content
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
-</script>
+  import header from './components/header/header.vue'
+  export default{
+    components: {
+      'v-header': header
+    }
+  }
+    // export default {
+    //   name: 'app'
+    // }
+</script> 
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .tab{
+    display:flex;
+    width:100%;
+    height:40px;
+    line-height:40px;
+    text-align: center;
+    
+        
+  }
+  .tab-item{
+      flex:1;
+      text-align: center;
+    }
+      
+      
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<<template>
+	<div class="header">
+		header模块
+	</div>
+</template>
+
+<script>
+</script> 
+	export default {}
+<style>
+</style>

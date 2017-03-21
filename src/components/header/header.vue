76ba72260f275aaa178ed3f01615dbc0f5cbe6a4
yangchaoben <842293890@qq.com>
2017-03-21 22:33:18 +0800

    修改

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="header">
-		<div class="content-wrapper">
+		<div class="content-wrapper" @click="showDetail">
 			<div class="avatar">
 				<img width="64" height="64" :src="seller.avatar" />
 			</div>
@@ -19,8 +19,9 @@
 			</div>
 		</div>
 		<div class="bullentin-wrapper">
-			
+			好吃好吃好吃好吃好吃好吃
 		</div>
+		<div class="detail" v-show="detailShow" @click="showDetail"></div>
 	</div>
 </template>
 
@@ -32,10 +33,41 @@
 			}
 		},
 		created() {
-			console.log('111')
+
+		},
+		data() {
+			return {
+				detailShow: false
+			}
+		},
+		methods: {
+			showDetail: function() {
+				this.detailShow = !this.detailShow
+			}
 		}
 	}
 </script> 
 
 <style>
+	.header{
+		padding: 10px;
+		background: #ccc;
+	}
+	.avatar{
+		display: inline-block;
+	}
+	.content{
+		margin-left: 10px;
+		display: inline-block;
+	}
+	.detail{
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 100;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		background: rgba(7,17,27,0.8)
+	}
 </style>
